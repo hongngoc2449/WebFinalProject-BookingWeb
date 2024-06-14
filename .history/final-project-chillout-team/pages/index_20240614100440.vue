1f85c9a385b684600423a9148c8b36bcb7984fc1
Way-curious <tuyentieple@gmail.com>
2024-06-14 10:06:53 +0700

    finish product component

--- .history/final-project-chillout-team/pages/index_20240614100440.vue
+++ .history/final-project-chillout-team/pages/index_20240614100440.vue
@@ -71,9 +71,9 @@ const products = [
   },
   {
     id: 9,
-    title: "ohemian Bedside Table",
+    title: "Shotwell Dresser",
     description: "This is product 9",
-    url: "https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-6028397840170_lifestyle?$rl_enh_1x1_zoom$",
+    url: "https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-60200431A2480_lifestyle?$rl_enh_1x1_zoom$",
     price: 92400,
   },
   {
