dd9fdac8abd0d5f5892a08b64890191e0a8dd7aa
Tuyen Trung Le <letrungtuyen2002@gmail.com>
2024-06-24 19:05:46 +0700

    updating schema and data model

--- final-project-chillout-team/server/api/prisma/get-product-by-id/[id].js
+++ final-project-chillout-team/server/api/prisma/get-product-by-id/[id].js
@@ -2,8 +2,12 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-    let product = await prisma.products.findFirst({
-        where: { id: Number(event.context.params.id) }
-    })
-    return product
+  const productId = Number(event.context.params.id);
+
+  const product = await prisma.products.findUnique({
+    where: { id: productId },
+    include: { images: true } // Include related images
+  });
+
+  return product;
 })
