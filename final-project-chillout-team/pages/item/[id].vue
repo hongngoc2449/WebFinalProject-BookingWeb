56a579f49a7f7d5f2aa8bfc71b40a28cf93b84ef
Tuyen Trung Le <letrungtuyen2002@gmail.com>
2024-06-24 17:57:59 +0700

    updating schema and data models and item pages

--- final-project-chillout-team/pages/item/[id].vue
+++ final-project-chillout-team/pages/item/[id].vue
@@ -1,67 +1,69 @@
 <template>
   <MainLayout>
     <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-      <div class="md:flex gap-4 justify-between mx-auto w-full">
+      <div class="md:flex gap-6 justify-between mx-auto w-full">
         <div class="md:w-[40%]">
-          <img v-if="currentImage" class="rounded-lg object-fit" :src="currentImage" width="500" height="400" />
+          <img v-if="currentImage" class="rounded-lg object-cover" :src="currentImage" width="500" height="400" />
 
-          <div v-if="images[0] !== ''" class="flex items-center justify-center mt-2">
-            <div v-for="image in images">
+          <div
+            v-if="images.length > 0"
+            class="flex items-center justify-center mt-4 space-x-1 justify-between md:flex-nowrap"
+          >
+            <div v-for="image in images" :key="image">
               <img
-                @mouseover="currentImage = image"
-                @click="currentImage = image"
-                width="80"
-                class="rounded-md object-fit border-[3px] cursor-pointer"
-                :class="currentImage === image ? 'border-[#FF5353]' : ''"
-                :src="image"
+                @mouseover="currentImage = image.url"
+                @click="currentImage = image.url"
+                width="85"
+                class="rounded-md object-cover border-[3px] cursor-pointer"
+                :class="currentImage === image.url ? 'border-[#FF5353]' : 'border-transparent'"
+                :src="image.url"
                 loading="lazy"
               />
             </div>
           </div>
         </div>
-        <div class="md:w-[60%] bg-white p-4 rounded-lg shadow-md">
-          <div v-if="product && product.data">
-            <p class="text-lg font-semibold text-gray-800 mb-2">{{ product.data.title }}</p>
-            <p class="text-sm text-gray-600 mb-2 leading-relaxed">{{ product.data.description }}</p>
+        <div class="md:w-[55%] bg-white p-6 rounded-lg shadow-md flex flex-col justify-between">
+          <div>
+            <div v-if="product && product.data">
+              <p class="text-2xl font-semibold text-gray-800 mb-2">{{ product.data.title }}</p>
+              <p class="text-md text-gray-600 mb-4 leading-relaxed">{{ product.data.description }}</p>
+            </div>
+
+            <div class="flex items-center py-2">
+              <span class="flex items-center justify-center w-5 h-5 rounded-full bg-yellow-400 mr-2">
+                <Icon name="material-symbols:star-rounded" class="text-white" size="12" />
+              </span>
+              <p class="text-red-500 font-semibold text-sm">Extra 5% off</p>
+            </div>
+
+            <div class="flex items-center my-4">
+              <Icon name="ic:baseline-star" color="#FF5353" size="20" />
+              <Icon name="ic:baseline-star" color="#FF5353" size="20" />
+              <Icon name="ic:baseline-star" color="#FF5353" size="20" />
+              <Icon name="ic:baseline-star" color="#FF5353" size="20" />
+              <Icon name="ic:baseline-star" color="#FF5353" size="20" />
+              <span class="text-sm text-gray-600 font-light ml-2">5,213 Reviews · 1,000+ orders</span>
+            </div>
+
+            <div class="flex items-center my-4">
+              <div class="text-xl font-bold text-gray-800">$ {{ priceComputed }}</div>
+              <span class="bg-gray-100 border border-gray-300 text-orange-500 text-sm font-semibold px-2 py-1 rounded-sm ml-3"
+                >70% off</span
+              >
+            </div>
+
+            <p class="text-green-600 text-sm font-semibold my-2">Free 11-day delivery over ￡8.28</p>
+            <p class="text-green-600 text-sm font-semibold my-2">Free Shipping</p>
+
+            <p style="color: rgb(255, 255, 255)">This text is white too.</p>
           </div>
-
-          <div class="flex items-center py-1">
-            <span class="flex items-center justify-center w-4 h-4 rounded-full bg-yellow-400 mr-1">
-              <Icon name="material-symbols:star-rounded" class="text-white" size="10" />
-            </span>
-            <p class="text-red-500 font-semibold text-xs">Extra 5% off</p>
-          </div>
-
-          <div class="flex items-center my-2">
-            <Icon name="ic:baseline-star" color="#FF5353" size="16" />
-            <Icon name="ic:baseline-star" color="#FF5353" size="16" />
-            <Icon name="ic:baseline-star" color="#FF5353" size="16" />
-            <Icon name="ic:baseline-star" color="#FF5353" size="16" />
-            <Icon name="ic:baseline-star" color="#FF5353" size="16" />
-            <span class="text-xs text-gray-600 font-light ml-1">5,213 Reviews · 1,000+ orders</span>
-          </div>
-
-          <div class="flex items-center my-2">
-            <div class="text-lg font-bold text-gray-800">$ {{ priceComputed }}</div>
-            <span
-              class="bg-gray-100 border border-gray-300 text-orange-500 text-xs font-semibold px-1.5 py-1 rounded-sm ml-2"
-              >70% off</span
-            >
-          </div>
-
-          <p class="text-green-600 text-xs font-semibold my-1">Free 11-day delivery over ￡8.28</p>
-
-          <p class="text-green-600 text-xs font-semibold my-1">Free Shipping</p>
-
-          <p style="color: rgb(255, 255, 255)">This text is white too.</p>
-
+          
           <button
             @click="addToCart()"
             :disabled="isInCart"
-            class="mt-3 w-full px-5 py-2.5 rounded-lg text-white text-base font-semibold bg-gradient-to-r from-orange-500 to-yellow-500 transition-transform transform hover:scale-105 disabled:opacity-50"
+            class="mt-6 w-full px-6 py-3 rounded-lg text-white text-lg font-semibold bg-red-500 transition-transform transform hover:scale-105 disabled:opacity-50"
           >
-            <div v-if="isInCart">Is Added</div>
-            <div v-else>Add to Cart</div>
+            <div>{{ isInCart ? 'Is Added' : 'Add to Cart' }}</div>
           </button>
         </div>
       </div>
@@ -78,6 +80,7 @@ const route = useRoute();
 
 let product = ref(null);
 let currentImage = ref(null);
+const images = ref([]);
 
 onBeforeMount(async () => {
   product.value = await useFetch(`/api/prisma/get-product-by-id/${route.params.id}`);
@@ -86,7 +89,7 @@ onBeforeMount(async () => {
 watchEffect(() => {
   if (product.value && product.value.data) {
     currentImage.value = product.value.data.url;
-    images.value[0] = product.value.data.url;
+    images.value = product.value.data.images || [product.value.data.url]; // Use the images field from the product data
     userStore.isLoading = false;
   }
 });
@@ -108,14 +111,7 @@ const priceComputed = computed(() => {
   return "0.00";
 });
 
-const images = ref([
-  "",
-  "https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-6028398210197_alternate2?$rl_enh_1x1_zoom$",
-  "https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-6028398210197_alternate3?$rl_enh_1x1_zoom$",
-  "https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-6028398210197_alternate4?$rl_enh_1x1_zoom$",
-  "https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-6028398210211_lifestyle?$rl_enh_1x1_zoom$",
-  "https://www.optimized-rlmedia.io/is/image/PoloGSI/s7-6028398210211_alternate2?$rl_enh_1x1_zoom$",
-]);
+
 
 const addToCart = () => {
   userStore.cart.push(product.value.data);
